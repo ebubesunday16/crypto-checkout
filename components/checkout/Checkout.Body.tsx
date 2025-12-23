214682a8c63811bfb1a1721753e49b759ac6c9a4
Emmanuel Sunday <135783723+ebubesunday16@users.noreply.github.com>
2025-12-23 10:38:17 +0100

    addressed typescript error

--- components/checkout/Checkout.Body.tsx
+++ components/checkout/Checkout.Body.tsx
@@ -2,15 +2,21 @@
 import React, { ReactNode } from 'react'
 import { useCheckoutContext } from './Checkout';
 
+export type CheckoutViewProps = {
+  when: string;
+  children?: React.ReactNode;
+};
+
+
 const Body = ({ children }: {children: ReactNode}) => {
 
   const { selectedTransferType } = useCheckoutContext();
 
-  const childArray = React.Children.toArray(children) as React.ReactElement[]
+  const childArray = React.Children.toArray(children) as React.ReactElement<CheckoutViewProps>[];
 
   const activeChild = childArray.find((child) => {
-    return child.props?.when === selectedTransferType
-  })
+    return child.props?.when === selectedTransferType 
+  }) 
 
 
   return (
